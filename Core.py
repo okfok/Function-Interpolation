ee1a8fcd98633bc938ef3a742068f44df9de9c94
okf0k <lubomvis@gmail.com>
2022-06-04 00:39:42 +0300

    rework: linear interpolation

--- Core.py
+++ Core.py
@@ -1,8 +1,8 @@
+import copy
 from typing import Callable
 
 import numpy as np
 from pydantic import BaseModel, Field
-from scipy import interpolate
 
 import config
 
@@ -24,17 +24,8 @@ class Point(BaseModel):
 class Interpolation(BaseModel):
     points: list[Point] = Field(default_factory=list)
 
-    def add_point(self, point: Point) -> None:
-        if point not in self.points:
-            self.points.append(point)
-            self.points.sort()
-
-    def remove_point(self, point: Point) -> None:
-        if point in self.points:
-            self.points.remove(point)
-
-    def clear_points(self) -> None:
-        self.points.clear()
+    def __str__(self):
+        return '\n'.join([str(point) for point in self.points])
 
     @property
     def x(self) -> np.ndarray:
@@ -44,17 +35,66 @@ class Interpolation(BaseModel):
     def y(self) -> np.ndarray:
         return np.array([point.y for point in self.points])
 
-    def get_interval(self):
+    @property
+    def min_x(self) -> float:
+        return self.points[0].x
+
+    @property
+    def max_x(self) -> float:
+        return self.points[-1].x
+
+    def _sort_points(self) -> None:
         self.points.sort()
-        min_ = self.points[0].x
-        max_ = self.points[-1].x
-        return np.linspace(min_, max_, int((max_ - min_) * config.GRAPH_ACCURACY))
+
+    def add_point(self, point: Point) -> None:
+        if point not in self.points:
+            self.points.append(point)
+            self._sort_points()
+
+    def remove_point(self, point: Point) -> None:
+        if point in self.points:
+            self.points.remove(point)
+
+    def delete_point(self, index: int) -> None:
+        if 0 <= index <= len(self.points):
+            self.points.pop(index)
+
+    def clear_points(self) -> None:
+        self.points.clear()
+
+    def x_validate(self, x) -> bool:
+        if isinstance(x, float):
+            return self.min_x <= x <= self.max_x
+        else:
+            return False
+
+    def get_interval(self) -> np.linspace:
+        self._sort_points()
+        return np.linspace(
+            self.min_x, self.max_x,
+            int((self.max_x - self.min_x) * config.GRAPH_ACCURACY)
+        )
 
     def get_linear_interpolation_func(self) -> Callable:
-        return interpolate.interp1d(self.x, self.y)
+        points = copy.deepcopy(self.points)
 
-    def get_cubic_interpolation_func(self) -> Callable:
-        return interpolate.interp1d(self.x, self.y, kind=3)
+        def linear_func(x0: float) -> float:
+            y1 = y2 = x1 = x2 = None
+            for i in range(len(points)):
+                if x0 == points[i].x:
+                    return points[i].y
+
+                if points[i].x < x0 < points[i + 1].x:
+                    x1 = points[i].x
+                    x2 = points[i + 1].x
+                    y1 = points[i].y
+                    y2 = points[i + 1].y
+
+            if None in (x1, x2, y1, y2):
+                raise ValueError()
+            return (y2 - y1) * (x0 - x1) / (x2 - x1) + y1
+
+        return linear_func
 
     def get_newton_interpolation_func(self) -> Callable:
         def table(x, y):
@@ -78,19 +118,13 @@ class Interpolation(BaseModel):
         quotients_table = table(self.x, self.y)
         corners = get_corner(quotients_table)
 
-        def newton_func(x_p):
+        def newton_func(x0: float):
             result = corners[0]
             for i in range(1, len(corners)):
                 p = corners[i]
                 for j in range(i):
-                    p *= (x_p - self.x[j])
+                    p *= (x0 - self.x[j])
                 result += p
             return result
 
         return newton_func
-
-    def __str__(self):
-        return '\n'.join([str(point) for point in self.points])
-
-
-interp = Interpolation()
